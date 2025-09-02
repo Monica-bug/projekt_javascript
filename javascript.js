524354a19234ebea483e5851e35563c5382ae4e0
Monica <mogensen69@gmail.com>
2025-09-03 00:49:56 +0200

    const
    tilføjelse af flere cards til inspiration

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
 console.log("JS kører korrekt!");
+
+const cardsData = [
+  { title: "Stue", description: "Se vores tips til en hyggelig stue.", img: "img/nordisk.jpg" },
+  { title: "Soveværelse", description: "Find inspiration til en god nattesøvn.", img: "img/sovevaerelse.jpg" },
+  { title: "Have", description: "Skab en skøn oase i haven.", img: "img/have.jpg" },
+  // Nye kort
+  { title: "Køkken", description: "Få idéer til et funktionelt køkken.", img: "img/koekken.jpg" },
+  { title: "Hyggelig belysning", description: "Indretning med hyggelig belysning.", img: "img/hyggeligbelysning.png" },
+  { title: "Badeværelse", description: "Inspiration til et flot badeværelse.", img: "img/bathroom1.jpg" }
+];
+
