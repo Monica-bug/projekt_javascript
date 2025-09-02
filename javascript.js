5e360bca434e68351c31682f2539505fd1704300
Monica <mogensen69@gmail.com>
2025-09-03 01:33:05 +0200

    rettet fejl
    fejl i koden bla. lukketag som gjorde at javascript ikke virkede og mappen var pludselig væk i visual code

--- javascript.js
+++ javascript.js
@@ -5,7 +5,6 @@ let totalClicks = 0;
 var currentFilter = "All";
 let menuOpen = false;
 
-
 const cardsData = [
   { title: "Stue", description: "Se vores tips til en hyggelig stue.", img: "img/nordisk.jpg" },
   { title: "Soveværelse", description: "Find inspiration til en god nattesøvn.", img: "img/sovevaerelse.jpg" },
@@ -23,7 +22,7 @@ const searchInput = document.querySelector(".search-bar input");
 function displayCards(filter) {
   cardsContainer.innerHTML = "";
 
-    for (let i = 0; i < cardsData.length; i++) {
+  for (let i = 0; i < cardsData.length; i++) {
     const card = cardsData[i];
 
     if (filter === "All" || card.title === filter) {
@@ -44,17 +43,21 @@ function displayCards(filter) {
 
       cardsContainer.appendChild(div);
     }
-    
+  }
+}
+
 // Første visning af alle kort
 displayCards(currentFilter);
-    
-      menuToggle.addEventListener("click", () => {
+
+// Menu toggle
+menuToggle.addEventListener("click", () => {
   menuOpen = !menuOpen; // boolean variabel
   console.log("Menu åben:", menuOpen);
   document.querySelector(".navbar").classList.toggle("open");
 });
 
-      searchInput.addEventListener("input", (e) => {
+// Søgefelt
+searchInput.addEventListener("input", (e) => {
   const searchTerm = e.target.value.toLowerCase();
   const filteredCards = cardsData.filter(card => card.title.toLowerCase().includes(searchTerm));
 
